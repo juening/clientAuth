8f6bcdbfc4b5d9dc76f1cac14b6227b757d0e412
Jue Ning <juening@gmail.com>
2017-04-16 11:24:29 -0400

    hoc to secure routes

--- src/index.js
+++ src/index.js
@@ -11,16 +11,29 @@ import reducers from './reducers';
 import Signin from './components/auth/signin';
 import SignOut from './components/auth/signout';
 import SignUp from './components/auth/signup';
+import Feature from './components/feature';
+import RequireAuth from './components/auth/requireAuth';
+import HomePage from './components/homePage';
+import types from './actions/types';
 
 const createStoreWithMiddleware = applyMiddleware(reduxThunk)(createStore);
+const store = createStoreWithMiddleware(reducers);
+
+const token = localStorage.getItem('token');
+//if token is available, consider the user logged 
+if(token) {
+  store.dispatch({ type: types.AUTH_USER });
+}
 
 ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
+  <Provider store={store}>
     <Router history={browserHistory} >
       <Route path='/' component={App}>
+        <IndexRoute component={HomePage} />
         <Route path='signin' component={Signin} />
         <Route path='signout' component={SignOut} />
         <Route path='signup' component={SignUp} />
+        <Route path='feature' component={ RequireAuth(Feature) } />
       </Route>
     </Router>
   </Provider>
